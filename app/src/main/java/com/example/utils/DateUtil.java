f53b4b41ae8891a8fd3602538cbf9db12aca1b1b
yuanyingqiu <1049852196@qq.com>
2016-07-01 16:13:11 +0800

    对mWeek的修改

--- app/src/main/java/com/example/utils/DateUtil.java
+++ app/src/main/java/com/example/utils/DateUtil.java
@@ -10,7 +10,7 @@ import java.util.Locale;
 public class DateUtil {
 
     private static final String[] mWeeks = new String[]{
-            "星期天","星期一","星期二","星期三","星期四","星期五","星期六"
+            "","星期天","星期一","星期二","星期三","星期四","星期五","星期六"
     };
 
 
