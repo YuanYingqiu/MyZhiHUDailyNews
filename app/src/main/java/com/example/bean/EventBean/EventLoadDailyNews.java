02ad7f8a474f0e7c4dd69f0f3556864b8373763d
YuanYingqiu <1049852196@qq.com>
2016-09-18 15:39:57 +0800

    commit

--- app/src/main/java/com/example/bean/EventBean/EventLoadDailyNews.java
+++ app/src/main/java/com/example/bean/EventBean/EventLoadDailyNews.java
@@ -6,4 +6,19 @@ public class EventLoadDailyNews extends BaseEventBean {
     public EventLoadDailyNews(int code,String msg){
         super(code,msg);
     }
+
+    public EventLoadDailyNews(int code , String msg , boolean isNeedRemoveAll){
+        super(code,msg);
+        this.isNeedRemoveAll = isNeedRemoveAll;
+    }
+
+    private boolean isNeedRemoveAll;
+
+    public boolean isNeedRemoveAll() {
+        return isNeedRemoveAll;
+    }
+
+    public void setNeedRemoveAll(boolean needRemoveAll) {
+        isNeedRemoveAll = needRemoveAll;
+    }
 }
